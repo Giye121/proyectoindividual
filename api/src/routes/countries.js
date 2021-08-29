3b93bbe1ecca4e70a5b04395878ce81186977625
Giye121 <jggrebe@gmail.com>
2021-08-29 16:08:56 -0400

    primercommit

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -3,16 +3,23 @@ const { Country } = require("../db");
 const router = Router();
 
 router.get("/", (req, res) => {
-  Country.findAll();
+  Country.findAll()
+    .then((country) => res.send(country))
+    .catch((error) => next(error));
+
+router.get("/:id", (req, res) => {
+  Country.findAll()
+    .then((country) => res.send(country))
+    .catch((error) => next(error));
+});
+
+router.post("/", (req, res) => {
   res.send("Ruta countries");
 });
-router.get("/", (req, res) => {
+router.put("/", (req, res) => {
   res.send("Ruta countries");
 });
-router.get("/", (req, res) => {
-  res.send("Ruta countries");
-});
-router.get("/", (req, res) => {
+router.delete("/", (req, res) => {
   res.send("Ruta countries");
 });
 
